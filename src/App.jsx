36a110e1a14c4aef1f060ace8e8d49699bf93286
kenzzo99 <aknez1406@gmail.com>
2023-04-01 16:24:23 -0400

    hero photo, icon fix, removed billing section

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => (
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
-        <Billing />
+        {/* <Billing /> */}
         <CardDeal />
         <Testimonials />
         <Clients />
